0583b8e065d3540f06b3f0c04aaa5f779d787216
AlevtMarshall <36171152+AlevtMarshall@users.noreply.github.com>
2018-03-02 15:49:50 +0300

    update_1 js

--- labconsult/js/main.js
+++ labconsult/js/main.js
@@ -6,6 +6,15 @@ $(function () {
         $('.menu').toggleClass('menu_active');
     });
 
+    // Прокрутка к секции
+    $('.go_to').click( function(){ // ловим клик по ссылке с классом go_to
+        var scroll_el = $(this).attr('href'); // возьмем содержимое атрибута href, должен быть селектором, т.е. например начинаться с # или .
+        if ($(scroll_el).length != 0) { // проверим существование элемента чтобы избежать ошибки
+            $('html, body').animate({ scrollTop: $(scroll_el).offset().top }, 1000); // анимируем скроолинг к элементу scroll_el
+        }
+        return false; // выключаем стандартное действие
+    });
+
 
     // Вызов модального окна
     $('.button__js').on('click', function(e) {
@@ -60,14 +69,7 @@ $(function () {
 
 
 
-    // Прокрутка к секции
-    $('.go_to').click( function(){ // ловим клик по ссылке с классом go_to
-        var scroll_el = $(this).attr('href'); // возьмем содержимое атрибута href, должен быть селектором, т.е. например начинаться с # или .
-        if ($(scroll_el).length != 0) { // проверим существование элемента чтобы избежать ошибки
-            $('html, body').animate({ scrollTop: $(scroll_el).offset().top }, 1000); // анимируем скроолинг к элементу scroll_el
-        }
-        return false; // выключаем стандартное действие
-    });
+    
     
     // Form send
         $('[data-submit]').on('click', function(e){
