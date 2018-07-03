7f43241a95ff042ac5fccde4a04e50e0ea771a71
AlevtMarshall <36171152+AlevtMarshall@users.noreply.github.com>
2018-07-03 18:51:49 +0300

    Natasha`s site
    +adaptive fixed

--- natasha-soyer/js/main.js
+++ natasha-soyer/js/main.js
@@ -11,7 +11,7 @@ $(document).click(function(event) {
     event.stopPropagation();
 });
 
-    $('.link__to, .btn__go').click( function(){ // ловим клик по ссылке с классом go_to
+    $('.link__to').click( function(){ // ловим клик по ссылке с классом go_to
         var scroll_el = $(this).attr('href'); // возьмем содержимое атрибута href, должен быть селектором, т.е. например начинаться с # или .
         if ($(scroll_el).length != 0) { // проверим существование элемента чтобы избежать ошибки
             $('html, body').animate({ scrollTop: $(scroll_el).offset().top }, 1500); // анимируем скроолинг к элементу scroll_el
